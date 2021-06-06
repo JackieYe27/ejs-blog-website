c26ec00c81a0ddb550d5d099b53282751dd0642e
Jackie Ye <jackieye27@gmail.com>
2021-06-05 18:52:32 -0700

    implemented a mongo database for data persistence

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,10 +15,27 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = []
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser:true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("we're connected!");
+});
+
+const blogSchema = new mongoose.Schema ({
+  postTitle: String,
+  postBody: String
+});
+
+const Post = mongoose.model("Post", blogSchema);
+
 
 app.get("/home", (req,res) => {
-  res.render("home", {homeStartingContent, posts});
+  Post.find((err,posts) => {
+    res.render("home", {homeStartingContent, posts});
+  });
 });
 
 app.get("/about", (req,res) => {
@@ -37,22 +55,20 @@ app.post("/compose", (req,res) => {
     postTitle:req.body.postTitle, 
     postBody:req.body.postBody
   };
-  posts.push(post);
-  res.redirect("/home");
-})
-
-app.get("/home/:postName", (req,res) => {
-  let requestTitle = _.lowerCase(req.params.postName);
-  posts.forEach(post => {
-    let storedTtitle = _.lowerCase(post.postTitle);
-    let storedBody = post.postBody;
-    if(storedTtitle === requestTitle) {
-      res.render("post", {postTitle:storedTtitle, postBody:storedBody})
-    } else {
-      console.log("Not a Match");
+  const postDB = new Post (post);
+  
+  postDB.save((err => {
+    if(!err) {
+      res.redirect("/home");
     }
-  });
-  res.redirect("/home");
+  }));
+});
+
+app.get("/home/:postId", (req,res) => {
+  const requestedPostId = req.params.postId;
+  Post.findOne({_id:requestedPostId}, (err, foundPost) => {
+    res.render("post", {postTitle: foundPost.postTitle, postBody:foundPost.postBody});
+  })
 })
 
 
